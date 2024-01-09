bc2244e71be930542227d1fee48ec6af6400aa57
hvqDevUnity <hoangvanquan2002.unitydevelop@gmail.com>
2024-01-10 01:42:14 +0700

    fix

--- api/moreOption/rp_day2day.php
+++ api/moreOption/rp_day2day.php
@@ -13,37 +13,41 @@ include_once('../../model/luotvaocua.php');
 $db = new db();
 $connect = $db->connect();
 $luotvaocua = new luotvaocua($connect);
-
-$data = json_decode(file_get_contents("php://input"));
-$luotvaocua->check_in = $data->check_in;
-$luotvaocua->check_out = $data->check_out;
-
-$read = $luotvaocua->rp_day2day();
-$num = $read->rowCount();
 $luotvaocua_array = [];
 
-if($num > 0){
-    $luotvaocua_array['data'] = [];
-
-    while($row = $read->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-
-        $luotvaocua_item = array(
-            'id_ve_xe' => $id_ve_xe,
-            'id_baigui' => $id_baigui,
-            'check_in' => $check_in,
-            'idnv_checkin' => $idnv_checkin,
-            'check_out' => $check_out,
-            'idnv_checkout' => $idnv_checkout,
-        );
-
-        array_push($luotvaocua_array['data'], $luotvaocua_item);
+if(isset($_GET["check_in"]) && isset($_GET["check_out"])){
+    $luotvaocua->check_in = $_GET["check_in"];
+    $luotvaocua->check_out = $_GET["check_out"];
+echo  $luotvaocua->check_in . "-".  $luotvaocua->check_out;
+    $read = $luotvaocua->rp_day2day();
+    $num = $read->rowCount();
+    
+    if($num > 0){
+        $luotvaocua_array['data'] = [];
+    
+        while($row = $read->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+    
+            $luotvaocua_item = array(
+                'id_ve_xe' => $id_ve_xe,
+                'id_baigui' => $id_baigui,
+                'check_in' => $check_in,
+                'idnv_checkin' => $idnv_checkin,
+                'check_out' => $check_out,
+                'idnv_checkout' => $idnv_checkout,
+            );
+    
+            array_push($luotvaocua_array['data'], $luotvaocua_item);
+        }
+    
+        $luotvaocua_array['message'] = ["complete"];
+    }else{
+        $luotvaocua_array['message'] = ["error"];
     }
-
-    $luotvaocua_array['message'] = ["complete"];
 }else{
     $luotvaocua_array['message'] = ["error"];
 }
+
 
 echo json_encode($luotvaocua_array);
 
